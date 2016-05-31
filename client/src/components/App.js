a177a2164fc810f96efd94968393887210d60a60
Dmitriy Borodiy <dmitriy.borodiy@gmail.com>
2016-05-31 17:03:17 +0300

    Switched to BrowserHistory; removed handling of different routes on backend

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -8,6 +8,7 @@ class App extends React.Component {
     render() {
         return (
             <div>
+                <Link to="/about">About</Link>
             {this.props.children}
             </div>
         )
